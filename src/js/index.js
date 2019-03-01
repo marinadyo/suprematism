b690c0e92e1f40d7293a636d1ab1486aab1b2bc4
Marinadyo <marinaandreevnadyo@gmail.com>
2019-03-01 18:22:09 +0300

    Сверстала все элементы картины

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import Parallax from 'parallax-js'
+import jQuery from "jquery";
+
+jQuery(function() {
+  var scene = document.getElementById('scene');
+  var parallaxInstance = new Parallax(scene);
+});
+
